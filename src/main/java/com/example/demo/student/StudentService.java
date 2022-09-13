bd5accdb001a5ff06ee75ad820e4275ab2414fdf
alicuhu <hoangnam.itus@gmail.com>
2022-09-13 09:03:37 +0700

    Implement PUT, Transactional update

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,6 +3,7 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,5 +37,21 @@ public class StudentService {
             );
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long id, String name, String email) {
+        Student student = studentRepository.findById(id).orElseThrow(() -> new IllegalStateException("student with id " + id + " does not exist"));
+        if (name != null && !name.isEmpty() && !name.equals(student.getName())) {
+            student.setName(name);
+        }
+        if (email != null && !email.isEmpty() && !email.equals(student.getName())) {
+            Optional<Student> foundStudent = studentRepository.findStudentByEmail(email);
+            if (foundStudent.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+        studentRepository.save(student);
     }
 }
